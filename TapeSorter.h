fe8c225cb44455da6a152f33d504487b779f3e80
Olesya <114682776+Olesya-Kosminskaya@users.noreply.github.com>
2024-05-21 21:58:57 +0300

    TapeSorter.h

--- TapeSorter.h
+++ TapeSorter.h
@@ -8,8 +8,11 @@ class TapeSorter
 private:
 public:
 
+// Разделение исходной ленты на временные (осуществляется по заданному объёму памяти)    
     size_t splitInputTape(Tape& inputTape, const std::string& outputTempTapeFilesName, const int& countElementsInTempTape);
+// Сортировка каждой из временных лент и запись в файлы
     void sortTmpTapes(const size_t& tempTapesCount, const std::string& outputTempTapeFilesName);
+// Общая сортировка и составление выходной ленты
     void sortToResultTape(Tape& outputTape, const size_t& tempTapesCount, const std::string& outputTempTapeFilesName);
 
 public:
